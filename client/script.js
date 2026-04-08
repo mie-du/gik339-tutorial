af071785b7cac03e542f6b59d23b9302771f4add
Mikaela Hedberg <mie@du.se>
2026-04-08 12:59:27 +0200

    removed support function

--- client/script.js
+++ client/script.js
@@ -82,8 +82,4 @@ const users = [
   }
 ];
 
-/* Stödfunktion för framtida funktionalitet */
-function getInitials(user) {
-  return `${user.firstName?.[0] ?? ''}${user.lastName?.[0] ?? ''}`.toUpperCase();
-}
 /* *** Nedan skriver vi resten av koden. *** */
